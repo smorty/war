55bae5e93a7c1430df34a9f97faa1828c394083a
Steve Mortensen <stevemortensen@smorte2-ml.local>
2014-05-22 08:11:14 -0600

    Fleshes out war game

--- script.js
+++ script.js
@@ -27,25 +27,63 @@ $(document).ready(function() {
 			deck.push({number: j+1, suit: suit});
 		}
 	}
+	// console.log(deck[0],deck[10],deck[51]);
 	
 	//shuffle the deck
+	deck = _.shuffle(deck);
+	// console.log(deck[0],deck[10],deck[51]);
 	
 	
 	var cards_player_1 = [];
 	var cards_player_2 = [];
+	var deal = function(cards,hand1,hand2){
+		for (var i = 0; i < cards.length; i++){
+			if (i % 2 === 0){
+				cards_player_1.push(cards[i]);
+			} else {
+				cards_player_2.push(cards[i]);
+			}
+		}
+		// console.log(cards_player_1[0],cards_player_1[25],cards_player_2[0],cards_player_2[25]);
+		return cards_player_1;
+		return cards_player_2;
+	};
+	deal(deck,cards_player_1,cards_player_2);
+	// console.log(cards_player_1[25],cards_player_2[25]);
 	//divide out the cards into the two arrays
 	
 	
 	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
-	function war() {
+	function war(card1,card2) {
+		if (card1.number > card2.number){
+			return card1;
+		} else if (card2.number > card1.number) {
+			return card2;
+		} else {
+			return "tie";
+		}
 	}
 	
+	// console.log(war(cards_player_1[0].number,cards_player_2[0].number));
 	
 	//create a play function
 		//compare the cards
 		//give the winner both cards (at end of deck)
 	function play() {
-		
+		// console.log(war(hand1[0],hand2[0]));
+		var result = war(cards_player_1[0],cards_player_2[0]);
+		if(result === cards_player_1[0]){
+			cards_player_1.push(cards_player_1.shift(), cards_player_2.shift());
+			console.log(result);
+		} else if (result === cards_player_2[0]){
+			cards_player_2.push(cards_player_1.shift(), cards_player_2.shift());
+			console.log(result);
+		} else if (result === "tie"){
+			console.log(result);
+			// debugger;
+			cards_player_1.push(cards_player_1.shift());
+			cards_player_2.push(cards_player_2.shift());
+		}
 		//this function (defined below) will continue to the next turn
 		advance();
 	}
